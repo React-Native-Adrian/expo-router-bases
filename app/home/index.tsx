c50177ba3aaf1bc402ca8580b0529e88cada2848
Adrian <alextravc.342@gmail.com>
2024-12-29 12:26:15 -0500

    ADD: cn tailwind merge function

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,43 @@
-import { Link } from 'expo-router';
-import { View, Text, SafeAreaView } from 'react-native';
+import { Link, router } from 'expo-router';
+import { SafeAreaView, View } from 'react-native';
+
+import CustomButton from '@/components/shared/CustomButton';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Home</Text>
+      <View className="px-10 mt-5">
+        <CustomButton
+          label="Products"
+          onPress={() => router.push('/products')}
+          className="mb-2"
+        />
 
-        <Link href="/products">Products</Link>
+        {/* -------- v2 with link req forwardRef -------- */}
+        <Link href="/profile" asChild>
+          <CustomButton label="Profile" className="mb-2" />
+        </Link>
+
+        {/* -------- variants -------- */}
+        <CustomButton
+          label="Settings"
+          variant="outlined"
+          color="tertiary"
+          onPress={() => router.push('/settings')}
+          className="mb-2"
+        />
+
+        <CustomButton
+          label="Home"
+          variant="text"
+          color="tertiary"
+          onPress={() => router.push('/home')}
+          className="mb-2"
+        />
+
+        {/* <Link href="/products">Products</Link>
         <Link href="/profile">Profile</Link>
-        <Link href="/settings">Settings</Link>
+        <Link href="/settings">Settings</Link> */}
       </View>
     </SafeAreaView>
   );
---
+++ components/shared/CustomButton.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Pressable, PressableProps, Text, View } from 'react-native';
+
+import { cn } from '@/utils/shared/cn';
+
+interface CustomButtonProps extends PressableProps {
+  label: string;
+  color?: 'primary' | 'secondary' | 'tertiary';
+
+  variant?: 'contained' | 'text' | 'outlined';
+  className?: string;
+}
+
+// forwardRef para usarlo en Link asChild
+const CustomButton = React.forwardRef<View, CustomButtonProps>(
+  (
+    { label, color = 'primary', variant = 'contained', className, ...rest },
+    ref
+  ) => {
+    const btnColor = {
+      primary: 'bg-primary',
+      secondary: 'bg-secondary',
+      tertiary: 'bg-tertiary',
+    }[color];
+    const btnOutlineColor = {
+      primary: 'border-primary',
+      secondary: 'border-secondary',
+      tertiary: 'border-tertiary',
+    }[color];
+
+    const textColor = {
+      primary: 'text-primary',
+      secondary: 'text-secondary',
+      tertiary: 'text-tertiary',
+    }[color];
+
+    if (variant === 'text') {
+      return (
+        <Pressable className={cn('p-3', className)} {...rest} ref={ref}>
+          <Text className={cn(textColor, 'text-center font-work-medium')}>
+            {label}
+          </Text>
+        </Pressable>
+      );
+    }
+
+    if (variant === 'outlined') {
+      return (
+        <Pressable
+          className={cn(
+            'p-3 rounded-md border',
+            btnOutlineColor,
+            textColor,
+            className
+          )}
+          {...rest}
+          ref={ref}
+        >
+          <Text className={cn(textColor, 'text-center font-work-medium')}>
+            {label}
+          </Text>
+        </Pressable>
+      );
+    }
+
+    return (
+      <Pressable
+        className={cn(
+          'p-3 rounded-md',
+          btnColor,
+          'active:opacity-90',
+          className
+        )}
+        {...rest}
+        ref={ref}
+      >
+        <Text className="text-white text-center font-work-medium">{label}</Text>
+      </Pressable>
+    );
+  }
+);
+
+export default CustomButton;
